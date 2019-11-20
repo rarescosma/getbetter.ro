5e6a201163da2cbe43b984b0ab0e68fd224320ea
Rareș Cosma <rares@getbetter.ro>
2019-11-20 09:14:53 +0100

    Blacken conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,20 +2,20 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Rareș Cosma'
-SITENAME = 'Getbetter'
+AUTHOR = "Rareș Cosma"
+SITENAME = "Getbetter"
 SITESUBTITLE = (
-   'A life journey of constant improvement in all areas of interests '
-   'by Rareș Cosma'
+    "A life journey of constant improvement in all areas of interests "
+    "by Rareș Cosma"
 )
-SITEURL = ''
-SITESRC = 'https://github.com/rarescosma/getbetter.ro'
+SITEURL = ""
+SITESRC = "https://github.com/rarescosma/getbetter.ro"
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'GMT'
+TIMEZONE = "GMT"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -26,27 +26,29 @@ AUTHOR_FEED_RSS = None
 
 # Biography
 BIO = "Senior software engineer / SRE<br/>Presently @ Klarna"
-PROFILE_IMAGE = 'avatar.png'
+PROFILE_IMAGE = "avatar.png"
 
 # Social widget
-SOCIAL = (('email', 'rares@getbetter.ro'),
-          ('linkedin', 'https://www.linkedin.com/in/rarescosma'),
-          ('github', 'https://github.com/rarescosma'))
+SOCIAL = (
+    ("email", "rares@getbetter.ro"),
+    ("linkedin", "https://www.linkedin.com/in/rarescosma"),
+    ("github", "https://github.com/rarescosma"),
+)
 
 DEFAULT_PAGINATION = 10
 
 # URL settings
-ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
-PAGE_URL = 'pages/{slug}/'
-PAGE_SAVE_AS = 'pages/{slug}/index.html'
+ARTICLE_URL = "{date:%Y}/{date:%m}/{date:%d}/{slug}/"
+ARTICLE_SAVE_AS = "{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html"
+PAGE_URL = "pages/{slug}/"
+PAGE_SAVE_AS = "pages/{slug}/index.html"
 
 # Theme
-THEME = 'themes/pelican-hyde'
+THEME = "themes/pelican-hyde"
 
 # Plugins
-PLUGIN_PATHS = ['plugins']
-PLUGINS = ['sitemap', 'summary', 'liquid_tags.getbetter', 'photos']
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["sitemap", "summary", "liquid_tags.getbetter", "photos"]
 
 # Photos
 PHOTO_LIBRARY = "~/src/getbetter.ro/var/galleries"
@@ -58,17 +60,13 @@ PHOTO_WATERMARK = False
 
 # Sitemap
 SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.5,
-        'indexes': 0.5,
-        'pages': 0.75,
-    },
-    'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'daily',
-        'pages': 'monthly',
+    "format": "xml",
+    "priorities": {"articles": 0.5, "indexes": 0.5, "pages": 0.75},
+    "changefreqs": {
+        "articles": "monthly",
+        "indexes": "daily",
+        "pages": "monthly",
     },
 }
 
-TEMPLATE_PAGES = {'wiki.html': 'wiki/index.html'}
+TEMPLATE_PAGES = {"wiki.html": "wiki/index.html"}
